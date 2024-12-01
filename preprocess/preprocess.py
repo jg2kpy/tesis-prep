7b5c64e6923e776fecfcc0c5fac4ca277bc902fd
jg2kpy <jlgutierrez2000@hotmail.com>
2024-12-01 17:04:23 -0300

    Preprocessing data

---
+++ preprocess/preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+#Leer el dataset
+df = pd.read_csv('../movielens-20m-dataset/rating.csv')
+
+#make the user ids go from 0...N-1
+df.userId = df.userId - 1
+
+# create a maping for movie ids
+unique_movie_ids = set(df.movieId.values)
+movie2idx = {}
+count = 0
+for movie_id in unique_movie_ids:
+    movie2idx[movie_id] = count
+    count += 1
+
+df['movie_idx'] = df.apply(lambda row: movie2idx[row.movieId], axis=1)
+
+df = df.drop(columns='timestamp')
+
+df.to_csv('./edited_rating.csv')
