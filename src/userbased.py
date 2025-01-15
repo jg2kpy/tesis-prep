5cce61773b5241615281592e61396daabd449a5c
jg2kpy <jlgutierrez2000@hotmail.com>
2025-01-15 18:54:34 -0300

    wip

--- src/userbased.py
+++ src/userbased.py
@@ -7,22 +7,22 @@ from datetime import datetime
 from sortedcontainers import SortedList
 
 import os
-if not os.path.exists('./preprocess/user2movie.json') or \
-    not os.path.exists('./preprocess/movie2user.json') or \
-    not os.path.exists('./preprocess/usermovie2rating.json') or \
-    not os.path.exists('./preprocess/usermovie2rating_test.json'):
+if not os.path.exists('./preprocess/data/user2movie.json') or \
+    not os.path.exists('./preprocess/data/movie2user.json') or \
+    not os.path.exists('./preprocess/data/usermovie2rating.json') or \
+    not os.path.exists('./preprocess/data/usermovie2rating_test.json'):
     import preprocess.preprocess2dict
 
-with open('./preprocess/user2movie.json', 'rb') as f:
+with open('./preprocess/data/user2movie.json', 'rb') as f:
     user2movie = pickle.load(f)
 
-with open('./preprocess/movie2user.json', 'rb') as f:
+with open('./preprocess/data/movie2user.json', 'rb') as f:
     movie2user = pickle.load(f)
 
-with open('./preprocess/usermovie2rating.json', 'rb') as f:
+with open('./preprocess/data/usermovie2rating.json', 'rb') as f:
     usermovie2rating = pickle.load(f)
 
-with open('./preprocess/usermovie2rating_test.json', 'rb') as f:
+with open('./preprocess/data/usermovie2rating_test.json', 'rb') as f:
     usermovie2rating_test = pickle.load(f)
 
 print(len(user2movie))
