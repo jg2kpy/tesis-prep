a562cb3d28083f7f95541368e45730aea848fc5d
jg2kpy <jlgutierrez2000@hotmail.com>
2025-04-19 17:21:24 -0300

    preprocesamiento modificado

--- src/HP-MOEA/preprocess.py
+++ src/HP-MOEA/preprocess.py
@@ -5,7 +5,7 @@ from sklearn.utils import shuffle
 
 count = 0
 
-def main(dataset_path, output_path = './data', top_usuarios = 10000, top_peliculas = 2000):
+def main(dataset_path, output_path = './data', top_usuarios = 10000, new_comer_filter = 5, random_seed = 69):
     # Leer el dataset
     print("\nLeyendo el dataset desde:", dataset_path)
     df = pd.read_csv(dataset_path)
@@ -31,36 +31,50 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
 
     print("\nTamaño del dataframe original:", len(df))
 
-    # Contar la cantidad de ratings por usuario y por película
-    print("Contando la cantidad de ratings por usuario y por película")
+
+    # Contar la cantidad de ratings por usuario
+    print("Contando la cantidad de ratings por usuario")
     user_ids_count = Counter(df.userId)
-    movie_ids_count = Counter(df.movie_idx)
 
-    # Seleccionar los usuarios y películas más comunes
-    print(f"Seleccionando los {top_usuarios} usuarios y {top_peliculas} películas más comunes")
+    # Seleccionar los usuarios más comunes
+    print(f"Seleccionando los {top_usuarios} usuarios películas más comunes")
     user_ids = [u for u, c in user_ids_count.most_common(top_usuarios)]
-    movie_ids = [u for u, c in movie_ids_count.most_common(top_peliculas)]
 
-    # Filtrar el dataframe para que solo contenga los usuarios y películas seleccionados
-    print("Filtrando el dataframe para que solo contenga los usuarios y películas seleccionados")
-    df_small = df[df.userId.isin(user_ids) & df.movie_idx.isin(movie_ids)].copy()
+    # Filtrar el dataframe para que solo contenga los usuarios seleccionados
+    print("Filtrando el dataframe para que solo contenga los usuarios seleccionados")
+    df_small = df[df.userId.isin(user_ids)].copy()
 
-    # Crear nuevos mapeos para los IDs de usuario y película
-    print("Creando nuevos mapeos para los IDs de usuario y película")
+    # Crear nuevos mapeos para los IDs de usuario
+    print("Creando nuevos mapeos para los IDs de usuario")
     new_user_id_map = {}
     i = 0
     for old in user_ids:
         new_user_id_map[old] = i
         i += 1
 
-    new_movie_id_map = {}
-    j = 0
-    for old in movie_ids:
-        new_movie_id_map[old] = j
-        j += 1
-
     df_small.loc[:, 'userId'] = df_small.apply(lambda row: new_user_id_map[row.userId], axis=1)
+
+    # Obtener los IDs de las películas que aparecen más de 5 veces y menos de 5 veces
+    movie_ids_count = Counter(df_small.movie_idx)
+    movies_more_than_5 = [movie_id for movie_id, count in movie_ids_count.items() if count > new_comer_filter]
+    new_comers = [movie_id for movie_id, count in movie_ids_count.items() if count <= new_comer_filter]
+
+    print(f"Películas que aparecen más de {new_comer_filter} veces: {len(movies_more_than_5)}")
+    print(f"Películas que aparecen menos o igual a {new_comer_filter} veces (new_comers): {len(new_comers)}")
+
+    print("Creando nuevos mapeos para los IDs de peliculas")
+    new_movie_id_map = {}
+    i = 0
+    for old in movies_more_than_5:
+        new_movie_id_map[old] = i
+        i += 1
+
+    for old in new_comers:
+        new_movie_id_map[old] = i
+        i += 1
+
     df_small.loc[:, 'movie_idx'] = df_small.apply(lambda row: new_movie_id_map[row.movie_idx], axis=1)
+
     print("ID máximo de usuario:", df_small.userId.max())
     print("ID máximo de película:", df_small.movie_idx.max())
 
@@ -68,10 +82,8 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
 
     # Barajar el dataframe
     print("\nBarajando el dataframe")
-    df = shuffle(df_small)
-    cutoff = int(0.8 * len(df))
-    df_train = df.iloc[:cutoff]
-    df_test = df.iloc[cutoff:]
+    df_shuffle = shuffle(df_small, random_state=random_seed)
+    df_shuffle_len = len(df_shuffle)
 
     user2movie = {}
     movie2user = {}
@@ -81,8 +93,8 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
     def update_user2movie_and_movie2user(row):
         global count
         count += 1
-        if count % 1000000 == 0:
-            print("Procesado: %.3f" % (float(count) / cutoff))
+        if count % 10000 == 0:
+            print(f'Procesado: {((float(count) / df_shuffle_len) * 100):.2f}%')
 
         i = int(row.userId)
         j = int(row.movie_idx)
@@ -98,21 +110,7 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
             movie2user[j].append(i)
 
         usermovie2rating[(i, j)] = row.rating
-    df_train.apply(update_user2movie_and_movie2user, axis=1)
-
-    usermovie2rating_test = {}
-    print("Llamando a: update_usermovie2rating_test")
-    count = 0
-    def udpate_usermovie2rating_test(row):
-        global count
-        count += 1
-        if count % 1000000 == 0:
-            print("Procesado: %.3f" % (float(count) / len(df_test)))
-
-        i = int(row.userId)
-        j = int(row.movie_idx)
-        usermovie2rating_test[(i, j)] = row.rating
-    df_test.apply(udpate_usermovie2rating_test, axis=1)
+    df_shuffle.apply(update_user2movie_and_movie2user, axis=1)
 
     def calcular_profit(ratings):
         if not ratings:
@@ -128,7 +126,7 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
         movie2profit[movie_id] = profit
 
     # Guardar los diccionarios en archivos
-    print("Guardando los diccionarios en archivos")
+    print("Guardando las estructuras de datos en archivos")
     with open(f'{output_path}/user2movie.pickle', 'wb') as f:
         pickle.dump(user2movie, f)
         print(f"user2movie guardado en {output_path}/user2movie.pickle")
@@ -141,13 +139,21 @@ def main(dataset_path, output_path = './data', top_usuarios = 10000, top_pelicul
         pickle.dump(usermovie2rating, f)
         print(f"usermovie2rating guardado en {output_path}/usermovie2rating.pickle")
 
-    with open(f'{output_path}/usermovie2rating_test.pickle', 'wb') as f:
-        pickle.dump(usermovie2rating_test, f)
-        print(f"usermovie2rating_test guardado en {output_path}/usermovie2rating_test.pickle")
-
     with open(f'{output_path}/movie2profit.pickle', 'wb') as f:
         pickle.dump(movie2profit, f)
         print(f"movie2profit guardado en {output_path}/movie2profit.pickle")
+
+    with open(f'{output_path}/new_comers.pickle', 'wb') as f:
+        pickle.dump(new_comers, f)
+        print(f"new_comers guardado en {output_path}/new_comers.pickle")
+
+    with open(f'{output_path}/new_user_id_map.pickle', 'wb') as f:
+        pickle.dump(new_user_id_map, f)
+        print(f"new_user_id_map guardado en {output_path}/new_user_id_map.pickle")
+
+    with open(f'{output_path}/new_movie_id_map.pickle', 'wb') as f:
+        pickle.dump(new_movie_id_map, f)
+        print(f"new_movie_id_map guardado en {output_path}/new_movie_id_map.pickle")
 
 if __name__ == "__main__":
     main()
